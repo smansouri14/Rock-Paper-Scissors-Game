1fc5779c7cc214bc5ee7424aa6c6f955bc1135ca
mansouri <mansouri@email.arizona.edu>
2020-03-29 19:41:44 -0500

    fixed scoreboard

--- js/app.js
+++ js/app.js
@@ -84,8 +84,8 @@ function showWinner(winner, computerChoice) {
     }
     // show Score
     score.innerHTML = `
-    <p>Player ${scoreboard.player}  : 
-    ${scoreboard.computer} Computer</p>
+    <p>Player: ${scoreboard.player}</p>  
+    <p>Computer: ${scoreboard.computer}</p>
     `;
 
     modal.style.display = 'block';
